f06f0f873e6ee835bef5188d1c2ec8ff55be4ada
Kong Huey Yee <erinkong84@gmail.com>
2019-07-26 21:57:27 +0800

    firebase sign in

---
+++ src/app/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB9WeoaRHPtOaQMWQSxuFabFrHVwMe2dPc",
+  authDomain: "event-manager-ionic-6f3ec.firebaseapp.com",
+  databaseURL: "https://event-manager-ionic-6f3ec.firebaseio.com",
+  projectId: "event-manager-ionic-6f3ec",
+  storageBucket: "",
+  messagingSenderId: "819313166462",
+  appId: "1:819313166462:web:baab169db9dcf86a"
+};
